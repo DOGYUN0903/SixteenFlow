d3f97c04b4568b22b97be2e6cded90078b33855a
SexyDoGyun <kimdk0903@naver.com>
2025-05-27 21:52:13 +0900

    Initial Commit

---
+++ src/main/java/com/newspeed/sixteenflow/SixteenFlowApplication.java
@@ -0,0 +1,13 @@
+package com.newspeed.sixteenflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixteenFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixteenFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newspeed/sixteenflow/SixteenFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newspeed.sixteenflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixteenFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
