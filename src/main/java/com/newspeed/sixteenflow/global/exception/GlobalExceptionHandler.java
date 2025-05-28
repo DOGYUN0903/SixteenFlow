f2895b7dcbb22dcfc0db891272440ad4de0bf18b
Jindnjs <wlsdnjs0310@naver.com>
2025-05-28 17:58:00 +0900

    :sparkles: feat: valid에러 메세지 처리 기능 추가

--- src/main/java/com/newspeed/sixteenflow/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/newspeed/sixteenflow/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.newspeed.sixteenflow.global.exception;
 
 import com.newspeed.sixteenflow.global.common.ApiResponse;
+import com.newspeed.sixteenflow.global.response.error.ValidationError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +15,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException e) {
         return ApiResponse.status(e.getErrorCode()).body();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String,String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ApiResponse.status(ValidationError.VALIDATION_FIELD_ERROR).body(errors);
     }
 }
