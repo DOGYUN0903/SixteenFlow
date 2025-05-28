0dedeaff67ee2d6456cd23cb56f29b55028d8db0
Jindnjs <wlsdnjs0310@naver.com>
2025-05-28 12:23:12 +0900

    :sparkles: feat: 공통 Exception 추가

---
+++ src/main/java/com/newspeed/sixteenflow/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.newspeed.sixteenflow.global.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BaseException.class)
+    public
+}
