c0ec6c1dea12ec34f0cd2b75e25d2871ff432155
Jindnjs <wlsdnjs0310@naver.com>
2025-05-28 15:01:46 +0900

    :sparkles: feat: 공통 응답 클래스, 공통 예외처리 작성

--- src/main/java/com/newspeed/sixteenflow/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/newspeed/sixteenflow/global/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.newspeed.sixteenflow.global.exception;
 
+import com.newspeed.sixteenflow.global.common.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
-    public
+    public ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException e) {
+        return ApiResponse.status(e.getErrorCode()).body();
+    }
 }
--- src/main/java/com/newspeed/sixteenflow/global/response/Test.java
+++
@@ -1,5 +0,0 @@
-package com.newspeed.sixteenflow.global.response;
-
-public class Test {
-    private String test;
-}
