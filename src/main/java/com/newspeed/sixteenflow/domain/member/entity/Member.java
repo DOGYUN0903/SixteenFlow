d961c58423f44c0fcb1a83d998d3a86fb99bddfa
Jang Gyeonghyuk <kcc5107@naver.com>
2025-05-29 03:26:59 +0900

    fix: 잘못 쓴 코드 삭제

--- src/main/java/com/newspeed/sixteenflow/domain/member/entity/Member.java
+++ src/main/java/com/newspeed/sixteenflow/domain/member/entity/Member.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
--- src/main/java/com/newspeed/sixteenflow/global/response/error/ValidationError.java
+++ src/main/java/com/newspeed/sixteenflow/global/response/error/ValidationError.java
@@ -5,9 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public enum ValidationError implements BaseCode {
     VALIDATION_FIELD_ERROR(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
-    MISSING_REQUEST_BODY(HttpStatus.BAD_REQUEST, "요청 본문이 비어있습니다."),
-    VALIDATION_PASSWORD_ERROR(HttpStatus.BAD_REQUEST,
-            "비밀번호는 최소 8글자 이상, 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야합니다.");
+    MISSING_REQUEST_BODY(HttpStatus.BAD_REQUEST, "요청 본문이 비어있습니다."),;
 
     private HttpStatus status;
     private String message;
