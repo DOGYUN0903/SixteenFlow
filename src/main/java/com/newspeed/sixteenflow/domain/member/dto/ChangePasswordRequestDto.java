1e60dfdf22c72707ef9435479e4d4be3f2a3ff53
Jang Gyeonghyuk <kcc5107@naver.com>
2025-05-29 19:50:45 +0900

    refactor: 오타 수정

--- src/main/java/com/newspeed/sixteenflow/domain/member/dto/ChangePasswordRequestDto.java
+++ src/main/java/com/newspeed/sixteenflow/domain/member/dto/ChangePasswordRequestDto.java
@@ -3,11 +3,11 @@ package com.newspeed.sixteenflow.domain.member.dto;
 import lombok.Getter;
 
 @Getter
-public class PasswordRequestDto {
+public class ChangePasswordRequestDto {
     private final String oldPassword;
     private final String newPassword;
 
-    public PasswordRequestDto(String oldPassword, String newPassword) {
+    public ChangePasswordRequestDto(String oldPassword, String newPassword) {
         this.oldPassword = oldPassword;
         this.newPassword = newPassword;
     }
