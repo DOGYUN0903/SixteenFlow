42d444e2ce9ad2f60b9778348e81cf3db6548d79
SexyDoGyun <kimdk0903@naver.com>
2025-05-29 12:04:52 +0900

    feat: 뉴스피드 조회(팔로우 한 사람의 피드만 조회) 구현

--- src/main/java/com/newspeed/sixteenflow/domain/post/controller/PostController.java
+++ src/main/java/com/newspeed/sixteenflow/domain/post/controller/PostController.java
@@ -25,7 +25,7 @@ public class PostController {
      * 게시글 생성
      */
     @PostMapping
-    public ResponseEntity<ApiResponse<CreatePostResponseDto>> createPost(@Valid @RequestBody CreatePostRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<CreatePostResponseDto>> create(@Valid @RequestBody CreatePostRequestDto requestDto) {
         // TODO: 인증 방식 결정 후 로그인 유저의 memberId 주입 로직 추가 예정
         // ex) @AuthenticationPrincipal Long memberId (Spring Security 사용 시)
         return ApiResponse.status(PostSuccess.POST_CREATED)
@@ -36,7 +36,7 @@ public class PostController {
      * 게시글 전체 조회
      */
     @GetMapping
-    public ResponseEntity<ApiResponse<PostListResponseDto>> findAllPost() {
+    public ResponseEntity<ApiResponse<PostListResponseDto>> findAll() {
         return ApiResponse.status(PostSuccess.POST_FOUND)
                 .body(postService.findAll());
     }
@@ -45,7 +45,7 @@ public class PostController {
      * 게시글 단건 조회
      */
     @GetMapping("/{postId}")
-    public ResponseEntity<ApiResponse<PostResponseDto>> findPostById(@PathVariable("postId") Long postId) {
+    public ResponseEntity<ApiResponse<PostResponseDto>> findById(@PathVariable("postId") Long postId) {
         return ApiResponse.status(PostSuccess.POST_FOUND)
                 .body(postService.findById(postId));
     }
@@ -54,7 +54,7 @@ public class PostController {
      * 게시글 수정
      */
     @PatchMapping("/{postId}")
-    public ResponseEntity<ApiResponse<UpdatePostResponseDto>> updatePost(@PathVariable("postId") Long postId,
+    public ResponseEntity<ApiResponse<UpdatePostResponseDto>> update(@PathVariable("postId") Long postId,
                                                                          @Valid @RequestBody UpdatePostRequestDto requestDto) {
         return ApiResponse.status(PostSuccess.POST_UPDATED)
                 .body(postService.update(6L, postId, requestDto));
@@ -64,9 +64,19 @@ public class PostController {
      * 게시글 삭제
      */
     @DeleteMapping("/{postId}")
-    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable("postId") Long postId) {
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable("postId") Long postId) {
         postService.delete(6L, postId);
         return ApiResponse.status(PostSuccess.POST_DELETED).body();
+    }
 
+    /**
+     * 뉴스피드 게시물 조회(팔로우 한 사람의 게시물만 조회)
+     */
+    @GetMapping("/feed")
+    public ResponseEntity<ApiResponse<PostListResponseDto>> getFollowingFeeds() {
+        // TODO: 실제 로그인 ID 주입 필요
+        Long memberId = 1L;
+        return ApiResponse.status(PostSuccess.POST_FOUND)
+                .body(postService.getFollowingFeeds(memberId));
     }
 }
--- src/main/java/com/newspeed/sixteenflow/domain/post/service/PostService.java
+++ src/main/java/com/newspeed/sixteenflow/domain/post/service/PostService.java
@@ -19,4 +19,6 @@ public interface PostService {
     UpdatePostResponseDto update(Long memberId, Long postId, UpdatePostRequestDto requestDto);
 
     void delete(Long memberId, Long postId);
+
+    PostListResponseDto getFollowingFeeds(Long memberId);
 }
