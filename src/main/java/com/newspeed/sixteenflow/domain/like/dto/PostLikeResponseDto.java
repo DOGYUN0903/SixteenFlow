6f30c86dc539b1409343c081649c11cb9af56d35
DaHyun-8509 <luce0359@gmail.com>
2025-05-29 19:51:26 +0900

    refactor: 주석 수정

--- src/main/java/com/newspeed/sixteenflow/domain/like/dto/PostLikeResponseDto.java
+++ src/main/java/com/newspeed/sixteenflow/domain/like/dto/PostLikeResponseDto.java
@@ -11,6 +11,6 @@ public class PostLikeResponseDto {
     public PostLikeResponseDto(Long postId, int likePostCount, boolean like) {
         this.postId = postId;
         this.likePostCount = likePostCount; // 좋아요 수
-        this.like = like; //좋아요 상태를 한번 더 알려줌 (없어도 됨)
+        this.like = like; //좋아요 상태를 한번 더 알려줌 (명시적으로 넣음)
     }
 }
