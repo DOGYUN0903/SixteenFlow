b92e69507b2fbb4a6f38ada3e566a88bbcc1c5ad
DaHyun-8509 <luce0359@gmail.com>
2025-05-30 16:56:02 +0900

    refactor: 게시글 좋아요 Dto 주석 추가 및, @RequireArgsConstructor 사용한 가독성 높이기

--- src/main/java/com/newspeed/sixteenflow/domain/like/dto/PostLikeResponseDto.java
+++ src/main/java/com/newspeed/sixteenflow/domain/like/dto/PostLikeResponseDto.java
@@ -1,16 +1,12 @@
 package com.newspeed.sixteenflow.domain.like.dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class PostLikeResponseDto {
-    private Long postId;
-    private int likePostCount;
-    private boolean like; // 현재 좋아요 상태 (toggle용)
-
-    public PostLikeResponseDto(Long postId, int likePostCount, boolean like) {
-        this.postId = postId;
-        this.likePostCount = likePostCount; // 좋아요 수
-        this.like = like; //좋아요 상태를 한번 더 알려줌 (명시적으로 넣음)
-    }
+    private final Long postId;
+    private final int likePostCount;
+    private final boolean like; // 현재 좋아요 상태 (toggle용)
 }
