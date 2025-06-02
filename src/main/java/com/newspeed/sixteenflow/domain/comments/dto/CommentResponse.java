f10e0f60f81fe4c86a79ba151790ed36568f2c4e
RanRanFry <burish111@gmail.com>
2025-06-02 19:59:49 +0900

    refactor :생성자 수정

--- src/main/java/com/newspeed/sixteenflow/domain/comments/dto/CommentResponse.java
+++ src/main/java/com/newspeed/sixteenflow/domain/comments/dto/CommentResponse.java
@@ -33,8 +33,8 @@ public class CommentResponse {
     }
 
     public CommentResponse(String nickName, String profileImageUrl, String content, LocalDateTime modifiedAt) {
-        nickName = nickName;
-        profileImageUrl = profileImageUrl;
+        this.nickName = nickName;
+        this.profileImageUrl = profileImageUrl;
         this.content = content;
         this.modifiedAt = modifiedAt;
     }
